0e2d23c4a6e8f109fd9ccfef12df1d1a46d0388c
Evan House <78841135+evanahouse@users.noreply.github.com>
2024-04-21 13:20:38 -0400

    added base theme

---
+++ src/constants/theme/theme.ts
@@ -0,0 +1,30 @@
+import {
+  BREAKPOINTS,
+  BUTTON_VARIANTS,
+  CARD_VARIANTS,
+  PALETTE,
+  SHADOW_VARIANTS,
+  SPACING,
+  TEXT_INPUT_VARIANTS,
+  TEXT_VARIANTS,
+  Z_INDICES,
+} from "@constants/theme";
+import { createTheme } from "@shopify/restyle";
+
+const BASE_THEME = {
+  colors: PALETTE,
+  spacing: SPACING,
+  widths: SPACING,
+  heights: SPACING,
+  borders: SPACING,
+  borderRadii: SPACING,
+  textVariants: TEXT_VARIANTS,
+  textInputVariants: TEXT_INPUT_VARIANTS,
+  cardVariants: CARD_VARIANTS,
+  buttonVariants: BUTTON_VARIANTS,
+  shadowVariants: SHADOW_VARIANTS,
+  breakpoints: BREAKPOINTS,
+  zIndices: Z_INDICES,
+};
+
+const theme = createTheme(BASE_THEME);
